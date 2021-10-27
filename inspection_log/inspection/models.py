7cb90c18996a9255e7f98c8f3107204308b778c1
fox-sv <Liahovkost@gmail.com>
2021-10-27 22:39:32 +0300

    fix model field record

--- inspection_log/inspection/models.py
+++ inspection_log/inspection/models.py
@@ -13,7 +13,7 @@ class Substation(models.Model):
 class Inspection_log(models.Model):
     date_record = models.DateField(verbose_name='Date of Record')
     job_type = models.CharField(max_length=100)
-    record = models.TextField(max_length=200, verbose_name='Record', blank=True)
+    record = models.TextField(max_length=2000, verbose_name='Record', blank=True)
     user_name_id = models.ForeignKey(User, verbose_name='Name', related_name='log_to_user', null=True, blank=True, on_delete=models.SET_NULL)
     substations = models.ForeignKey(Substation, verbose_name='substation', related_name='log_to_substation', null=True, blank=True, on_delete=models.SET_NULL)
     substation_name = models.CharField(max_length=100, verbose_name='substation')
